fc5b4a93506a8ab503b6b9340072a293776d40ee
Kisei Tanaka <1595661320113302@mil>
2022-12-08 15:47:32 -1000

    ok

--- script/1_Read_GFW_fleet_daily.R
+++ script/1_Read_GFW_fleet_daily.R
@@ -7,15 +7,15 @@ library(ggplot2)
 
 rm(list = ls())
 
-# first download GFW files from Google Drive folder
+# first download GFW files from Google Drive folder and unzip them on your G drive
 # https://drive.google.com/drive/folders/13jvlzDvJqhFxCTrrv-osajmosKL6fAmv?usp=sharing
 
 fv = read_csv("G:/GFW/fishing-vessels-v2.csv")
 
-daily_list = list.files("G:/GFW/fleet-daily-csvs-100-v2-2020/", pattern = ".csv")
+daily_list = list.files("G:/GFW/fleet-daily_2012_2020/", pattern = ".csv")
 
 # https://marineregions.org/gazetteer.php?p=details&id=26866
-mpa = raster("data/MarineRegions-worldheritagemarineprogramme.tif")
+mpa = raster("data/mpa/MarineRegions-worldheritagemarineprogramme.tif")
 mpa = rasterToPoints(mpa) %>% as.data.frame()
 mpa$x = ifelse(mpa$x < 0, mpa$x + 360, mpa$x)
 colnames(mpa)[3] = "z"
@@ -29,23 +29,31 @@ ggplot() +
   geom_raster(data = mpa, aes(x, y, fill = factor(z))) + 
   scale_fill_viridis_d()
 
+load("data/SURVEY_MASTER.RData"); sm = df %>% subset(island %in% c("Howland", "Baker")); sm$lon = ifelse(sm$lon < 0, sm$lon + 360, sm$lon)
+
 df = vector("list")
 
 for (i in 1:length(daily_list)) {
   
   # i = 1
   
-  df_i = read_csv(paste0("G:/GFW/fleet-daily-csvs-100-v2-2020/", daily_list[i]))
+  df_i = read_csv(paste0("G:/GFW/fleet-daily_2012_2020/", daily_list[i]))
   
   df_i$cell_ll_lon = ifelse(df_i$cell_ll_lon < 0, df_i$cell_ll_lon + 360, df_i$cell_ll_lon)
   
-  df_i = df_i %>% subset(cell_ll_lon > 183 & cell_ll_lon < 192 & cell_ll_lat > -8 & cell_ll_lat < 1 )
+  df_i = df_i %>% subset(cell_ll_lon > 180 & cell_ll_lon < 188 & cell_ll_lat > -8 & cell_ll_lat < 8 )
   
+  # plot(df_i$cell_ll_lon, df_i$cell_ll_lat)
+  # points(sm$lon, sm$lat, col = 2)
+
   df[[i]] = df_i
   print(i)
+  
 }
 
 df = rbindlist(df)
+
+save(df, file = "data/gfw/fleet_daily_2012-2020.rdata")
 
 df %>% 
   mutate(year = lubridate::year(date), 
